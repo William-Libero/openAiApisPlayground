2b40bfe8852bd4c5bf2707f677d9e02b391421f5
William Líbero <william.libero@dev.spacemoon.com.br>
2024-02-01 20:27:24 -0300

    Alterado valor inicial da store do pinia que controla a exibição de components.

--- src/stores/currentPage.js
+++ src/stores/currentPage.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useCurrentPageStore = defineStore("currentPage", () => {
-  const currentPage = ref(null);
+  const currentPage = ref("chat_completion");
   function changeCurrentPage(currentPage) {
     currentPage.value = currentPage;
   }
