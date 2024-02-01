aa8649a2f28256f5c9ee6faf10ebe4b75855303b
William Líbero <william.libero@dev.spacemoon.com.br>
2024-02-01 20:01:10 -0300

    Alteração na variavel de ambiente.

--- src/plugins/openai.js
+++ src/plugins/openai.js
@@ -1,8 +1,12 @@
+import CryptoJS from "crypto-js";
+
+var cript = import.meta.env.VITE_OPENAI_API_KEY;
+var decript = CryptoJS.AES.decrypt(cript, "secret key 123");
+var decryptedData = decript.toString(CryptoJS.enc.Utf8);
+
 import OpenAI from "openai";
 
-console.log(process.env);
-
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: decryptedData,
   dangerouslyAllowBrowser: true,
 });
